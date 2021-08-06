3f79f24a754dd470ff29a9a036231b06fd606a79
Cleyson Lima <cleysonph@gmail.com>
2021-08-06 01:42:07 -0300

    04:07 - refatora metodo de criação de emprestimos

--- src/main/java/br/com/treinaweb/twbiblioteca/services/EmprestimoService.java
+++ src/main/java/br/com/treinaweb/twbiblioteca/services/EmprestimoService.java
@@ -9,13 +9,18 @@ import br.com.treinaweb.twbiblioteca.models.Obra;
 
 public class EmprestimoService {
 
+    private static final int DIAS_PARA_DEVOLUCAO = 3;
+
     public Emprestimo novo(Cliente cliente, List<Obra> obras) {
         var emprestimo = new Emprestimo();
 
+        var dataEmprestimo = LocalDate.now();
+        var dataDevolucao = dataEmprestimo.plusDays(DIAS_PARA_DEVOLUCAO);
+
         emprestimo.setCliente(cliente);
         emprestimo.setLivros(obras);
-        emprestimo.setDataEmprestimo(LocalDate.now());
-        emprestimo.setDataDevolucao(LocalDate.now().plusDays(3));
+        emprestimo.setDataEmprestimo(dataEmprestimo);
+        emprestimo.setDataDevolucao(dataDevolucao);
 
         return emprestimo;
     }
