6dd715deb1453b3e9bd173dfb6a19b5d62f18c11
Cleyson Lima <cleysonph@gmail.com>
2021-08-06 02:20:12 -0300

    04:08 - refatora regra que define quantidade de dias para devolução

--- src/main/java/br/com/treinaweb/twbiblioteca/enums/Reputacao.java
+++ src/main/java/br/com/treinaweb/twbiblioteca/enums/Reputacao.java
@@ -1,5 +1,22 @@
 package br.com.treinaweb.twbiblioteca.enums;
 
 public enum Reputacao {
-    RUIM, REGULAR, BOA;
+    RUIM {
+        @Override
+        public int obterDiasParaDevolucao() {
+            return 1;
+        }
+    }, REGULAR {
+        @Override
+        public int obterDiasParaDevolucao() {
+            return 3;
+        }
+    }, BOA {
+        @Override
+        public int obterDiasParaDevolucao() {
+            return 5;
+        }
+    };
+
+    public abstract int obterDiasParaDevolucao();
 }
--- src/main/java/br/com/treinaweb/twbiblioteca/services/EmprestimoService.java
+++ src/main/java/br/com/treinaweb/twbiblioteca/services/EmprestimoService.java
@@ -3,7 +3,6 @@ package br.com.treinaweb.twbiblioteca.services;
 import java.time.LocalDate;
 import java.util.List;
 
-import br.com.treinaweb.twbiblioteca.enums.Reputacao;
 import br.com.treinaweb.twbiblioteca.models.Cliente;
 import br.com.treinaweb.twbiblioteca.models.Emprestimo;
 import br.com.treinaweb.twbiblioteca.models.Obra;
@@ -14,14 +13,7 @@ public class EmprestimoService {
         var emprestimo = new Emprestimo();
 
         var dataEmprestimo = LocalDate.now();
-        var diasParaDevolucao = 3;
-        if (cliente.getReputacao() == Reputacao.RUIM) {
-            diasParaDevolucao = 1;
-        } else if (cliente.getReputacao() == Reputacao.REGULAR) {
-            diasParaDevolucao = 3;
-        } else {
-            diasParaDevolucao = 5;
-        }
+        var diasParaDevolucao = cliente.getReputacao().obterDiasParaDevolucao();
         var dataDevolucao = dataEmprestimo.plusDays(diasParaDevolucao);
 
         emprestimo.setCliente(cliente);
